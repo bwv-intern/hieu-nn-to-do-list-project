e974ecbc7096fa6cefe5c700f3ecbc271a72056f
H-LAP-00060\hieu.nn <hieu.nn@briswell-vn.com>
2026-04-22 16:27:14 +0700

    feat: implement UserRepositoryInterface and UserRepository #4

---
+++ app/Repositories/UserRepository.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Support\Facades\Hash;
+
+class UserRepository implements UserRepositoryInterface {
+
+    // Create a new user
+    public function create(array $data) {
+
+        return User::create([
+            'name' => data['name'],
+            'email' => data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+    }
+
+    public function findByEmail(string $email) {
+
+        return User::where('email', $email)->first();
+
+    }
+
+}
