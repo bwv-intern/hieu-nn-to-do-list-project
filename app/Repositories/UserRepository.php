666b5fc48d1f2d46a7f8c6be69b01443f33de126
H-LAP-00060\hieu.nn <hieu.nn@briswell-vn.com>
2026-04-23 09:58:32 +0700

    fix: add missing $ to data variable in UserRepository #4

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -12,8 +12,8 @@ class UserRepository implements UserRepositoryInterface {
     public function create(array $data) {
 
         return User::create([
-            'name' => data['name'],
-            'email' => data['email'],
+            'name' => $data['name'],
+            'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
 
