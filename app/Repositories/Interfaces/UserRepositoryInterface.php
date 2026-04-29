1a0554aff7f19f57eeced6e930b9b4c0d9ad5b97
H-LAP-00060\hieu.nn <hieu.nn@briswell-vn.com>
2026-04-29 13:16:54 +0700

    feat: add check_email  function for remote AJAX jQuery #26

--- app/Repositories/Interfaces/UserRepositoryInterface.php
+++ app/Repositories/Interfaces/UserRepositoryInterface.php
@@ -7,4 +7,7 @@ interface UserRepositoryInterface {
     public function create(array $data);
 
     public function findByEmail(string $email);
+
+    // Check email for jQuery Remote
+    public function checkEmail(string $email);
 }
